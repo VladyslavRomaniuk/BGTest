97289646b89d2b44f6288dc7f2c4f38a8692bb3d
vlad.romanyuk <prostovlad871@gmail.com>
2023-10-16 02:56:54 +0300

    Add project files.

---
+++ BGLocalWeb/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using BGLocalWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BGLocalWeb.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
